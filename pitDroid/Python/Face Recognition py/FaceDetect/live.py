333d7f4a3a1dc9c7287a064563049f01e165925e
Sergio Polimante <sergiopolimante@gmail.com>
2017-06-06 19:24:43 -0300

    repositorios corrigidos

---
+++ pitDroid/Python/Face Recognition py/FaceDetect/live.py
@@ -0,0 +1,50 @@
+# This script will detect faces via your webcam.
+# Tested with OpenCV3
+
+import cv2
+import serial # if you have not already done so
+
+
+ser = serial.Serial(2, 9600)  #abre serial COM3
+
+cap = cv2.VideoCapture(0)
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+        #flags = cv2.CV_HAAR_SCALE_IMAGE
+    )
+
+    print("Found {0} faces!".format(len(faces)))
+    print(faces)
+    
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        ser.write("foi!")
+
+# When everything done, release the capture
+ser.close()
+cap.release()
+cv2.destroyAllWindows()
